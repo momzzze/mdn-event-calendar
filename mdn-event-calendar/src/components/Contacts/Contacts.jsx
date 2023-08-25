d1278281045389986402c1f5c7ff1a5df4b4a34e
nikola.ninov <nikola@ninov.org>
2023-08-26 00:53:03 +0300

    #48,#49 add users from db implement searchbar

--- mdn-event-calendar/src/components/Contacts/Contacts.jsx
+++ mdn-event-calendar/src/components/Contacts/Contacts.jsx
@@ -3,41 +3,61 @@ import { useForm } from "react-hook-form";
 import { useAuth } from "../../contexts/AuthContext";
 import { createContactList } from "../../services/contacts.service";
 import { useData } from "../../contexts/DataContext";
+import { FaUser } from "react-icons/fa";
 
 const Contacts = () => {
-    const dummyUsers = [
-        { id: 1, name: 'John Doe', avatar: 'https://images.pexels.com/photos/810775/pexels-photo-810775.jpeg' },
-        { id: 2, name: 'Jane Smith', avatar: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' },
-        { id: 3, name: 'Alex Johnson', avatar: 'https://images.pexels.com/photos/1906157/pexels-photo-1906157.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' },
-    ];
+
     const { users, contactLists } = useData();
     const [isAddingList, setIsAddingList] = useState(false);
     const [newListName, setNewListName] = useState('');
     const { register, reset, handleSubmit } = useForm();
     const { userData } = useAuth();
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const handleSearchChange = (event) => {
+        setSearchTerm(event.target.value);
+    };
+
     const handleAddListClick = () => {
         setIsAddingList(!isAddingList);
     };
     const handleAddListSubmit = async (data) => {
-        console.log({ name: data?.listName, userid: userData?.uid });
         const createdList = await createContactList(data?.listName, userData?.uid);
         setIsAddingList(false);
         setNewListName('');
         reset();
     };
 
+    const filteredUsers = users
+        ? users.filter((user) =>
+            user.username.toLowerCase().includes(searchTerm.toLowerCase())
+        )
+        : [];
+
     return (
         <div className="flex h-screen bg-gray-100  border-2 ">
             <div className="w-2/3 bg-white p-4 shadow-md">
-                <input type="text" placeholder="Search users" className="w-full px-3 py-2 mb-4 border rounded" />
+                <input
+                    type="text"
+                    placeholder="Search users..."
+                    value={searchTerm}
+                    onChange={handleSearchChange}
+                    className="w-full px-3 py-2 mb-4 border rounded"
+                />
                 <h2 className="text-xl font-semibold mb-2">Users</h2>
                 <div className="grid place-items-center w-70% max-h-96 overflow-y-auto">
                     <ul className="w-3/4">
-                        {dummyUsers.map(user => (
-                            <li key={user.id} className="bg-white rounded-lg p-4 flex items-center justify-between mb-4">
+                        {filteredUsers?.map(user => (
+                            <li key={user.id} className="bg-white rounded-lg p-4 flex bg-purple-50 items-center justify-between mb-4">
                                 <div className="flex items-center space-x-2">
-                                    <img src={user.avatar} alt={`${user.name} Avatar`} className="w-10 h-10 rounded-full mr-10" />
-                                    <span className="text-lg font-semibold">{user.name}</span>
+                                    {user.photo ? (
+                                        <img src={user.photo} alt={`${user?.username} Avatar`} className="w-10 h-10 rounded-full mr-10" />
+                                    ) : (
+                                        <div className="w-10 h-10 bg-purple-800 rounded-full mr-10 flex items-center justify-center">
+                                            <FaUser size={20} color="white" />
+                                        </div>
+                                    )}
+                                    <span className="text-lg font-semibold">{user.firstName} {user.lastName}</span>
                                 </div>
                                 <button className="px-4 py-2 bg-purple-800 hover:bg-purple-300 text-white rounded">
                                     Add to Contacts
@@ -47,7 +67,7 @@ const Contacts = () => {
                     </ul>
                 </div>
             </div>
-            <div className="w-1/3 bg-white p-4 shadow-md">
+            <div className="w-1/3 bg-white p-4 shadow-md bg-purple-50">
                 <h3 className="text-lg font-semibold mb-2">Your Contact Lists</h3>
                 <ul className="space-y-2 mb-6 mt-6">
                     {contactLists && contactLists.map(contactList => (
