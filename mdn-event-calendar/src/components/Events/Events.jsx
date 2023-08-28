db5b8f71201781d644d0d8e2527a5fcb38150fd4
nikola.ninov <nikola@ninov.org>
2023-08-28 12:08:50 +0300

    fix blur efect on singin sginup

--- mdn-event-calendar/src/components/Events/Events.jsx
+++ mdn-event-calendar/src/components/Events/Events.jsx
@@ -5,7 +5,7 @@ const Events=()=>{
         <div className="container mx-auto bg-indigo-500 h-screen">
             <h1>Events</h1>
 
-            <div class='container mx-auto px-4 m-20 bg-[#50d71e]'>
+            <div className='container mx-auto px-4 m-20 bg-[#50d71e]'>
                 <h2>This is the list of events!</h2>
 
             </div>
