9d5d1b666df8cca364483a761686fb3204551855
nikola.ninov <nikola@ninov.org>
2023-08-28 11:18:59 +0300

    Update Contacts.jsx

--- mdn-event-calendar/src/components/Contacts/Contacts.jsx
+++ mdn-event-calendar/src/components/Contacts/Contacts.jsx
@@ -156,13 +156,22 @@ const Contacts = () => {
                                 </div>
                                 <button
                                     onClick={() => handleAddContactClick(user.uid)}
-                                    className={`px-4 py-2 bg-purple-800 text-white rounded ${sendingInvites.some(invite => invite.receiver === user.uid && invite.status === 'pending')
-                                        ? 'bg-gray-300 cursor-not-allowed'
-                                        : 'hover:bg-purple-300'
+                                    className={`px-4 py-2 bg-purple-800 text-white rounded ${sendingInvites && sendingInvites.some(
+                                        invite => invite.receiver === user.uid && invite.status === 'pending'
+                                    )
+                                            ? 'bg-gray-300 cursor-not-allowed'
+                                            : 'hover:bg-purple-300'
                                         }`}
-                                    disabled={sendingInvites.some(invite => invite.receiver === user.uid && invite.status === 'pending')}
+                                    disabled={
+                                        sendingInvites &&
+                                        sendingInvites.some(
+                                            invite => invite.receiver === user.uid && invite.status === 'pending'
+                                        )
+                                    }
                                 >
-                                    {sendingInvites.some(invite => invite.receiver === user.uid && invite.status === 'pending')
+                                    {sendingInvites && sendingInvites.some(
+                                        invite => invite.receiver === user.uid && invite.status === 'pending'
+                                    )
                                         ? 'Pending'
                                         : 'Add to Contacts'}
                                 </button>
@@ -299,33 +308,37 @@ const Contacts = () => {
                     {userContacts && (
                         <div>
                             <h3 className="text-lg font-semibold mb-2">Your Contacts</h3>
-                            {Object.values(userContacts).map((contactId) => {
-                                const contactData = users.find((user) => user.uid === contactId);
-                                if (contactData) {
-                                    return (
-                                        <div key={contactId} className="flex items-center justify-center mb-3">
-                                            <div className="flex justify-between items-center w-4/5 bg-purple-100 rounded-full">
-                                                {contactData.photo ? (
-                                                    <img src={contactData.photo} alt={`${contactData.username} Avatar`} className=" ml-6 w-10 h-10 rounded-full mr-2" />
-                                                ) : (
-                                                    <div className="w-10 h-10 bg-purple-800 rounded-full mr-10 flex items-center justify-center ml-6">
-                                                        <FaUser size={20} color="white" />
-                                                    </div>
-                                                )}
-                                                <span className="text-lg font-semibold mr-6">{contactData.username}</span>
-                                                <button
-                                                    className="text-red-500 hover:text-red-700 mr-6"
-                                                    onClick={() => removeContactHandle(contactId)}
-                                                >
-                                                    <FaTimes />
-                                                </button>
+                            {users ? (
+                                Object.values(userContacts).map((contactId) => {
+                                    const contactData = users.find((user) => user.uid === contactId);
+                                    if (contactData) {
+                                        return (
+                                            <div key={contactId} className="flex items-center justify-center mb-3">
+                                                <div className="flex justify-between items-center w-4/5 bg-purple-100 rounded-full">
+                                                    {contactData.photo ? (
+                                                        <img src={contactData.photo} alt={`${contactData.username} Avatar`} className=" ml-6 w-10 h-10 rounded-full mr-2" />
+                                                    ) : (
+                                                        <div className="w-10 h-10 bg-purple-800 rounded-full mr-10 flex items-center justify-center ml-6">
+                                                            <FaUser size={20} color="white" />
+                                                        </div>
+                                                    )}
+                                                    <span className="text-lg font-semibold mr-6">{contactData.username}</span>
+                                                    <button
+                                                        className="text-red-500 hover:text-red-700 mr-6"
+                                                        onClick={() => removeContactHandle(contactId)}
+                                                    >
+                                                        <FaTimes />
+                                                    </button>
+                                                </div>
                                             </div>
-                                        </div>
-                                    );
-                                } else {
-                                    return null;
-                                }
-                            })}
+                                        );
+                                    } else {
+                                        return null;
+                                    }
+                                })
+                            ) : (
+                                <p>Loading users...</p>
+                            )}
                         </div>
                     )}
 
