0f83bb7f3de9e7714ee6ce7360070475c3337131
Denitsa Georgieva <denitsa.y@abv.bg>
2023-09-05 18:47:56 +0300

    fix: change hour format in ScheduleListItem

--- mdn-event-calendar/src/components/Calendar/Schedule/ScheduleListItem.jsx
+++ mdn-event-calendar/src/components/Calendar/Schedule/ScheduleListItem.jsx
@@ -5,8 +5,8 @@ import { useNavigate } from "react-router-dom";
 const ScheduleListItem = ({ event, selectDate }) => {
   const [isHovered, setIsHovered] = useState(false);
   const navigate = useNavigate();
-  const startDate = dayjs(event?.startDate).format("MMM D, h:mm A");
-  const endDate = dayjs(event?.endDate).format("MMM D, h:mm A");
+  const startDate = dayjs(event?.startDate).format("MMM D, HH:mm");
+  const endDate = dayjs(event?.endDate).format("MMM D, HH:mm");
 
   const handleCardClick = () => {
     navigate(`/event/${event?.id}`,
@@ -20,22 +20,23 @@ const ScheduleListItem = ({ event, selectDate }) => {
 
   return (
     <li
-      className={`flex items-center px-4 py-2 space-x-4 group border-2 bg-white rounded-xl
+      className={`flex items-center px-4 py-2 space-x-4 group border-2 bg-white rounded-xl text-white
       ${isHovered ? "scale-105 cursor-pointer" : ""}`}
       onClick={handleCardClick}
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
+      style={{ backgroundColor: event?.color }}
     >
       <div className="flex flex-col items-center">
         <img
-          src={event.imageUrl}
+          src={event?.imageUrl ? event?.imageUrl : 'https://png.pngtree.com/png-clipart/20190916/original/pngtree-flat-calendar-png-image_4590004.jpg'}
           alt="event image"
           className="object-cover flex-none w-12 h-12 rounded-full"
         />
-        <p className="mt-0.5 text-gray-500">{event?.publicity}</p>
+        <p className="mt-0.5">{event?.publicity}</p>
       </div>
       <div className="flex-auto">
-        <p className="text-lg text-gray-900 font-bold">{event.title}</p>
+        <p className="text-lg font-bold">{event.title}</p>
         <p className="mt-0.5">
           <span className="mt-0.5 mr-3 font-bold">Start</span>
           <time dateTime={event?.startDate}>{startDate}</time>
