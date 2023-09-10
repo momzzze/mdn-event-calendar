0ba1b8b755ce1847dfdad4576ce048ec0d9e4589
nikola.ninov <nikola@ninov.org>
2023-09-10 09:41:19 +0300

    Update WeatherApp.jsx

--- mdn-event-calendar/src/components/Weather/WeatherApp.jsx
+++ mdn-event-calendar/src/components/Weather/WeatherApp.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { fetchWeatherData } from '../../services/weather.service';
-import { FaSun, FaCloud, FaCloudSun, FaCloudMoon, FaCloudRain, FaCloudShowersHeavy } from 'react-icons/fa';
+import { FaSun, FaCloud, FaCloudSun, FaCloudRain, FaCloudShowersHeavy } from 'react-icons/fa';
 import { IoMdSunny } from 'react-icons/io';
 
 const WeatherApp = ({ city }) => {
@@ -21,7 +21,7 @@ const WeatherApp = ({ city }) => {
     const weatherIcons = {
         'sunny': <FaSun />,
         'partly cloudy': <FaCloudSun />,
-        'clear': <FaSun />,
+        'clear': <IoMdSunny />,
         'overcast': <FaCloud />,
         'cloudy': <FaCloud />,
         'rain': <FaCloudRain />,
@@ -31,50 +31,73 @@ const WeatherApp = ({ city }) => {
     };
 
     return (
-        <div className="bg-gray-200  py-8 ml-5 mr-5 mb-16 ">
-            <div className="max-w-screen-lg bg-white mx-auto p-8  ml-5 mr-5">
-                {weatherData && (
-                    <div >
-                        <div className="flex flex-col lg:flex-row lg:items-center bg-purple-800 rounded-full">
-                            <div className="lg:w-1/2">
-                                <h2 className="text-2xl font-semibold mb-2 text-white">
-                                    Weather in {weatherData.location.name}
-                                </h2>
-                                <p className="text-lg text-white">
-                                    Temperature: {weatherData.current.temp_c}°C
-                                </p>
-                            </div>
-                            <div className="lg:w-1/2 flex justify-center items-center">
-                                <div className="text-8xl text-white">
-                                    {weatherIcons[weatherData.current.condition.text.toLowerCase()]}
-                                </div>
-                            </div>
+        <div className="flex justify-center">
+            <div className="card min-w-md max-w-4xl border border-gray-100 bg-gray-50 transition-shadow test shadow-lg hover:shadow-shadow-xl w-full bg-purple-700 text-purple-50 rounded-md p-6">
+                <h2 className="text-lg mb-4">
+                    <div className="flex justify-between">
+                        <div className="badge relative top-0">
+                            <span className="text-lg font-semibold">
+                                {weatherData.location.name}
+                            </span>
                         </div>
-                        <div className="flex items-center justify-center mt-4 flex-col lg:flex-row lg:items-center space-x-6">
-                            {weatherData.forecast.forecastday.map((day, index) => {
-                                const dateObj = new Date(day.date);
-                                const dayOfWeek = dateObj.toLocaleDateString('en-US', { weekday: 'short' });
-                                const weatherIcon = weatherIcons[day.day.condition.text.toLowerCase()] || <span><FaCloud /></span>;
-
-                                return (
-                                    <div key={index} className="flex items-center space-x-2 mb-2 md:mb-0">
-                                        <div className="text-2xl text-purple-800">
-                                            {weatherIcon}
-                                        </div>
-                                        <div>
-                                            <div className="text-lg text-purple-800">
-                                                {day.day.avgtemp_c}°C
-                                            </div>
-                                            <div className="text-sm text-purple-800">
-                                                {dayOfWeek}
-                                            </div>
-                                        </div>
-                                    </div>
-                                );
-                            })}
+                        <span className="text-xl font-bold">
+                            {weatherData.current.last_updated}
+                        </span>
+                    </div>
+                </h2>
+                <div className="flex items-center justify-center">
+                    <div className="flex justify-center items-center w-32 h-32">
+                        <div className="text-9xl">
+                            {weatherIcons[weatherData.current.condition.text.toLowerCase()]}
                         </div>
                     </div>
-                )}
+                </div>
+                <div className="text-xl pt-4 pb-4">
+                    <div className="flex justify-between items-center">
+                        <div className="space-y-4">
+                            <span className="flex space-x-2 items-center">
+                                <svg height="24" width="24" viewBox="0 0 32 32" className="fill-current">
+                                    <path d="M13,30a5.0057,5.0057,0,0,1-5-5h2a3,3,0,1,0,3-3H4V20h9a5,5,0,0,1,0,10Z"></path>
+                                    <path d="M25 25a5.0057 5.0057 0 01-5-5h2a3 3 0 103-3H2V15H25a5 5 0 010 10zM21 12H6V10H21a3 3 0 10-3-3H16a5 5 0 115 5z"></path>
+                                </svg>
+                                <span>{weatherData.current.wind_kph} km/h</span>
+                            </span>
+                            <span className="flex space-x-2 items-center">
+                                <svg height="24" width="24" viewBox="0 0 32 32" className="fill-current">
+                                    <path d="M16,24V22a3.2965,3.2965,0,0,0,3-3h2A5.2668,5.2668,0,0,1,16,24Z"></path>
+                                    <path d="M16,28a9.0114,9.0114,0,0,1-9-9,9.9843,9.9843,0,0,1,1.4941-4.9554L15.1528,3.4367a1.04,1.04,0,0,1,1.6944,0l6.6289,10.5564A10.0633,10.0633,0,0,1,25,19,9.0114,9.0114,0,0,1,16,28ZM16,5.8483l-5.7817,9.2079A7.9771,7.9771,0,0,0,9,19a7,7,0,0,0,14,0,8.0615,8.0615,0,0,0-1.248-3.9953Z"></path>
+                                </svg>
+                                <span>{weatherData.current.humidity}%</span>
+                            </span>
+                        </div>
+                        <div>
+                            <h1 className="text-7xl">{weatherData.current.temp_c}°</h1>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex items-center justify-center mt-4 flex-col lg:flex-row lg:items-center space-x-4">
+                    {weatherData.forecast.forecastday.map((day, index) => {
+                        const dateObj = new Date(day.date);
+                        const dayOfWeek = dateObj.toLocaleDateString('en-US', { weekday: 'short' });
+                        const weatherIcon = weatherIcons[day.day.condition.text.toLowerCase()] || <span><FaCloud /></span>;
+
+                        return (
+                            <div key={index} className="flex items-center space-x-2 mb-2 md:mb-0">
+                                <div className="text-4xl text-white">
+                                    {weatherIcon}
+                                </div>
+                                <div>
+                                    <div className="text-2xl text-white">
+                                        {day.day.avgtemp_c}°C
+                                    </div>
+                                    <div className="text-lg text-white">
+                                        {dayOfWeek}
+                                    </div>
+                                </div>
+                            </div>
+                        );
+                    })}
+                </div>
             </div>
         </div>
     );
